68658627be4f792a699d18ca54eb332cfba70c42
DemoEto <paintcillssz@gmail.com>
2024-09-07 15:43:09 +0700

    Add: wallboard

---
+++ frontend/coreui-free-react-admin-template/src/wbconfig.js
@@ -0,0 +1,20 @@
+
+const wbconfig = {
+    development: {
+        hosturl  : 'https://10.21.43.203:8105/api',
+        wsurl    : 'wss://10.21.43.203:8105',
+        masterKey     : 'wallboardapi',
+        clientKey     : 'wallboardapi',
+        javascriptKey : 'wallboardapi',
+        appId : "wallboardapi"
+    },
+    production: {
+        hosturl  : 'https://10.21.43.203:8105/api',
+        wsurl    : 'wss://10.21.43.203:8105',
+        masterKey     : 'wallboardapi',
+        clientKey     : 'wallboardapi',
+        javascriptKey : 'wallboardapi',
+        appId : "wallboardapi"
+    }
+};
+export default wbconfig;
